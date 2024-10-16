0950aea76d110c8d063c83b818d8c6e9d2dd9eae
TinhMai123 <Tinhmvqse173245@fpt.edu.vn>
2024-10-16 17:25:19 +0700

    making User

--- KoiOrderingSystem_BusinessObject/Data/KoiOrderingSystemContext.cs
+++ KoiOrderingSystem_BusinessObject/Data/KoiOrderingSystemContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,34 +10,51 @@ namespace KoiOrderingSystem_BusinessObject.Data
 {
     public class KoiOrderingSystemContext : DbContext
     {
-        public KoiOrderingSystemContext() { }   
+        public KoiOrderingSystemContext() { }
         public KoiOrderingSystemContext(DbContextOptions<KoiOrderingSystemContext> options)
         : base(options)
         {
 
         }
 
-        DbSet<Currency> Currencies { get; set; }
-        DbSet<Farm> Farms { get; set; }
-        DbSet<Koi> Kois { get; set; }
-        DbSet<KoiByBatch> KoiByBatches { get; set; }
-        DbSet<KoiType> KoiTypes { get; set; }
-        DbSet<FarmKoiType> FarmKoiTypes { get; set; }
-        DbSet<Feedback> Feedbacks { get; set; }
-        DbSet<Insurance> Insurances { get; set;}
-        DbSet<User> Users { get; set; }
-        DbSet<Payment> Payments { get; set; }
-        DbSet<Order> Orders { get; set; }
-        DbSet<OrderDetailKoi> OrderDetails { get; set; }
-        DbSet<OrderKoi> OrderKois { get; set; }
-        DbSet<OrderTrip> OrderTrips { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetConnectionString());
+            }
+        }
+
+        private string GetConnectionString()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            return configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        }
+
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<Farm> Farms { get; set; }
+        public DbSet<Koi> Kois { get; set; }
+        public DbSet<KoiByBatch> KoiByBatches { get; set; }
+        public DbSet<KoiType> KoiTypes { get; set; }
+        public DbSet<FarmKoiType> FarmKoiTypes { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Insurance> Insurances { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetailKoi> OrderDetails { get; set; }
+        public DbSet<OrderKoi> OrderKois { get; set; }
+        public DbSet<OrderTrip> OrderTrips { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Order>()
-                .HasOne(o => o.Customer) 
-                .WithMany(u => u.Orders) 
+                .HasOne(o => o.Customer)
+                .WithMany(u => u.Orders)
                 .HasForeignKey(o => o.CustomerId);
             modelBuilder.Entity<Farm>()
                 .HasOne(o => o.Manager)
@@ -53,14 +71,14 @@ namespace KoiOrderingSystem_BusinessObject.Data
                 .OnDelete(DeleteBehavior.Restrict);
             modelBuilder.Entity<OrderDetailKoi>()
                 .HasOne(o => o.OrderKoi)
-                .WithMany(u => u.OrderDetailKois) 
+                .WithMany(u => u.OrderDetailKois)
                 .HasForeignKey(o => o.OrderKoiId)
                 .OnDelete(DeleteBehavior.Restrict);
             modelBuilder.Entity<OrderDetailKoi>()
                 .HasOne(o => o.Koi)
                 .WithOne(u => u.OrderDetailKoi)
                 .HasForeignKey<OrderDetailKoi>(o => o.KoiId)
-                .OnDelete(DeleteBehavior.Restrict); 
+                .OnDelete(DeleteBehavior.Restrict);
             modelBuilder.Entity<OrderDetailKoi>()
                 .HasOne(o => o.KoiByBatch)
                 .WithOne(u => u.OrderDetailKoi)
---
+++ KoiOrderingSystem_Service/UserService.cs
@@ -0,0 +1,30 @@
+﻿using KoiOrderingSystem_BusinessObject;
+using KoiOrderingSystem_Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiOrderingSystem_Service
+{
+    public class UserService : IUserService
+    {
+        private IUserRepo IUserRepo;
+
+
+        public UserService()
+        {
+            IUserRepo = new UserRepo();
+        }
+        public User? GetUserByEmail(string email)
+        {
+            return IUserRepo.GetUserByEmail(email);
+        }
+
+        public List<User> GetUsers()
+        {
+            return IUserRepo.GetUsers();
+        }
+    }
+}
