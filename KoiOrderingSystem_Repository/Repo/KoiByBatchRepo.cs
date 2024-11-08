b903b280de9648f0000b925782b47b7299d7596e
Shymastic <shymastic@gmail.com>
2024-11-08 17:33:02 +0700

    changing KoiByBatchRepo

--- KoiOrderingSystem_Repository/Repo/KoiByBatchRepo.cs
+++ KoiOrderingSystem_Repository/Repo/KoiByBatchRepo.cs
@@ -7,11 +7,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOrderingSystem_DAO;
 
 namespace KoiOrderingSystem_Repository.Repo
 {
     public class KoiByBatchRepo : IKoiByBatchRepo
     {
+        public async Task<bool> Add(KoiByBatch model)
+        {
+            return await KoiByBatchDAO.Instance.Add(model);
+        }
 
+        public async Task<List<KoiByBatch>> GetAll()
+        {
+            return await KoiByBatchDAO.Instance.GetAll();
+        }
+
+        public async Task<KoiByBatch?> GetById(int id)
+        {
+            return await KoiByBatchDAO.Instance.GetById(id);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await KoiByBatchDAO.Instance.Remove(id);
+        }
+
+        public async Task<bool> Update(KoiByBatch model)
+        {
+            return await KoiByBatchDAO.Instance.Update(model);
+        }
     }
 }
