90f8a30d9b7715dd073ec6e6f2d01c289333e3b7
Shymastic <shymastic@gmail.com>
2024-11-08 17:31:33 +0700

    changing InsuranceRepo

--- KoiOrderingSystem_Repository/Repo/InsuranceRepo.cs
+++ KoiOrderingSystem_Repository/Repo/InsuranceRepo.cs
@@ -7,11 +7,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOrderingSystem_DAO;
 
 namespace KoiOrderingSystem_Repository.Repo
 {
-    public class InsuranceRepo :  IInsuranceRepo
+    public class InsuranceRepo : IInsuranceRepo
     {
+        public async Task<bool> Add(Insurance model)
+        {
+            return await InsuranceDAO.Instance.Add(model);    
+        }
 
+        public async Task<List<Insurance>> GetAll()
+        {
+            return await InsuranceDAO.Instance.GetAll();
+        }
+
+        public async Task<Insurance?> GetById(int id)
+        {
+            return await InsuranceDAO.Instance.GetById(id);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await InsuranceDAO.Instance.Remove(id);
+        }
+
+        public async Task<bool> Update(Insurance model)
+        {
+            return await InsuranceDAO.Instance.Update(model);
+        }
     }
 }
