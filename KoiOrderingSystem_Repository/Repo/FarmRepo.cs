426b3597a9d98b5050abb0b2d0646749e63afd96
Shymastic <shymastic@gmail.com>
2024-11-08 17:28:45 +0700

    changing FarmRepo

--- KoiOrderingSystem_Repository/Repo/FarmRepo.cs
+++ KoiOrderingSystem_Repository/Repo/FarmRepo.cs
@@ -10,5 +10,29 @@ namespace KoiOrderingSystem_Repository.Repo
 {
     public class FarmRepo : IFarmRepo
     {
+        public async Task<bool> Add(Farm model)
+        {
+            return await FarmDAO.Instance.Add(model);
+        }
+
+        public async Task<List<Farm>> GetAll()
+        {
+            return await FarmDAO.Instance.GetAll();
+        }
+
+        public async Task<Farm?> GetById(int id)
+        {
+            return await FarmDAO.Instance.GetById(id);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await FarmDAO.Instance.Remove(id);
+        }
+
+        public async Task<bool> Update(Farm model)
+        {
+            return await FarmDAO.Instance.Update(model);
+        }
     }
 }
