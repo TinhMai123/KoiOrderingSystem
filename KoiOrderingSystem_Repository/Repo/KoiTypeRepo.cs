3933e27a7ea96135ff089141641b7855f545470c
Shymastic <shymastic@gmail.com>
2024-11-08 17:35:47 +0700

    changing KoiTypeDAO

--- KoiOrderingSystem_Repository/Repo/KoiTypeRepo.cs
+++ KoiOrderingSystem_Repository/Repo/KoiTypeRepo.cs
@@ -7,8 +7,31 @@ using System.Collections.Generic;
 
 namespace KoiOrderingSystem_Repository.Repo
 {
-    public class KoiTypeRepo :  IKoiTypeRepo
+    public class KoiTypeRepo : IKoiTypeRepo
     {
+        public async Task<bool> Add(KoiType model)
+        {
+            return await KoiTypeDAO.Instance.Add(model);
+        }
 
+        public async Task<List<KoiType>> GetAll()
+        {
+            return await KoiTypeDAO.Instance.GetAll();
+        }
+
+        public async Task<KoiType?> GetById(int id)
+        {
+            return await KoiTypeDAO.Instance.GetById(id);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await KoiTypeDAO.Instance.Remove(id);
+        }
+
+        public async Task<bool> Update(KoiType model)
+        {
+            return await KoiTypeDAO.Instance.Update(model);
+        }
     }
 }
