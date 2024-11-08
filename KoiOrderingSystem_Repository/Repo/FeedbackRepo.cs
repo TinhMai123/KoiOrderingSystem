0956c3a9ba227ea7bf181d094b72d5bda9d2dd2b
Shymastic <shymastic@gmail.com>
2024-11-08 17:30:08 +0700

    changing FeedbackRepo

--- KoiOrderingSystem_Repository/Repo/FeedbackRepo.cs
+++ KoiOrderingSystem_Repository/Repo/FeedbackRepo.cs
@@ -7,10 +7,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOrderingSystem_DAO;
 
 namespace KoiOrderingSystem_Repository.Repo
 {
     public class FeedbackRepo : IFeedbackRepo
     {
+        public async Task<bool> Add(Feedback model)
+        {
+           return await FeedbackDAO.Instance.Add(model);
+        }
+
+        public async Task<List<Feedback>> GetAll()
+        {
+            return await FeedbackDAO.Instance.GetAll();
+        }
+
+        public async Task<Feedback?> GetById(int id)
+        {
+            return await FeedbackDAO.Instance.GetById(id);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await FeedbackDAO.Instance.Remove(id);
+        }
+
+        public async Task<bool> Update(Feedback model)
+        {
+            return await FeedbackDAO.Instance.Update(model);
+        }
     }
 }
