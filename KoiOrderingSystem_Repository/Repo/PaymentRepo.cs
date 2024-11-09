642a634cd5a8bfb5c52594f586f8466240357359
Shymastic <shymastic@gmail.com>
2024-11-09 18:49:45 +0700

    changing PaymentRepo

--- KoiOrderingSystem_Repository/Repo/PaymentRepo.cs
+++ KoiOrderingSystem_Repository/Repo/PaymentRepo.cs
@@ -7,11 +7,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOrderingSystem_DAO;
 
 namespace KoiOrderingSystem_Repository.Repo
 {
     public class PaymentRepo : IPaymentRepo
     {
+        public async Task<bool> Add(Payment model)
+        {
+            return await PaymentDAO.Instance.Add(model);
+        }
 
+        public async Task<List<Payment>> GetAll()
+        {
+            return await PaymentDAO.Instance.GetAll();
+        }
+
+        public async Task<Payment?> GetById(int id)
+        {
+            return await PaymentDAO.Instance.GetById(id);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await PaymentDAO.Instance.Remove(id);
+        }
+
+        public async Task<bool> Update(Payment model)
+        {
+            return await PaymentDAO.Instance.Update(model);
+        }
     }
 }
