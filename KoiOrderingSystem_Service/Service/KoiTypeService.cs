d7c25259e72ab3f3e20a7f15e15d38f4bf354641
Shymastic <shymastic@gmail.com>
2024-11-10 00:32:10 +0700

    update KoiTypeService

--- KoiOrderingSystem_Service/Service/KoiTypeService.cs
+++ KoiOrderingSystem_Service/Service/KoiTypeService.cs
@@ -12,15 +12,13 @@ namespace KoiOrderingSystem_Service.Service
     public class KoiTypeService : IKoiTypeService
     {
         
-        private readonly IBaseRepository<KoiType> _koiTypeRepository;
         private readonly IKoiTypeRepo _repo;
 
 
 
         // Constructor to initialize the repository
-        public KoiTypeService(IBaseRepository<KoiType> koiTypeRepository, IKoiTypeRepo repo)
+        public KoiTypeService( IKoiTypeRepo repo)
         {
-            _koiTypeRepository = koiTypeRepository;
             _repo = repo;
         }
 
@@ -31,19 +29,24 @@ namespace KoiOrderingSystem_Service.Service
             {
                 throw new ArgumentNullException(nameof(koiType), "KoiType cannot be null.");
             }
-            return await _koiTypeRepository.AddAsync(koiType);
+            var check = await _repo.ReadAll();
+            if (check.Any(c=>c.Name.ToLower().Equals(koiType.Name.ToLower())))
+            {
+                throw new Exception($"This name {koiType.Name} had already been taken");
+            }
+            return await _repo.Add(koiType);
         }
 
         // Retrieve all KoiType records
         public async Task<List<KoiType>> GetAlls()
         {
-            return await _koiTypeRepository.GetAllAsync();
+            return await _repo.GetAll();
         }
 
         // Retrieve a KoiType by ID
         public async Task<KoiType?> GetById(int id)
         {
-            return await _koiTypeRepository.GetByIdAsync(id);
+            return await _repo.GetById(id);
         }
 
         // Update an existing KoiType
@@ -53,13 +56,18 @@ namespace KoiOrderingSystem_Service.Service
             {
                 throw new ArgumentNullException(nameof(koiType), "KoiType cannot be null.");
             }
-            return await _koiTypeRepository.UpdateAsync(koiType);
+            var check = await _repo.ReadAll();
+            if (check.Any(c => c.Name.ToLower().Equals(koiType.Name.ToLower()) && c.Name != koiType.Name))
+            {
+                throw new Exception($"This name {koiType.Name} had already been taken");
+            }
+            return await _repo.Update(koiType);
         }
 
         // Delete a KoiType by ID
         public async Task<bool> DeleteAsync(int id)
         {
-            return await _koiTypeRepository.DeleteAsync(id);
+            return await _repo.Remove(id);
         }
 
         
