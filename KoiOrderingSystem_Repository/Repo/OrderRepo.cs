49e6755f030b3df1af2dc6441c56b5c9f9cb5bf7
Shymastic <shymastic@gmail.com>
2024-11-09 18:46:34 +0700

    changing OrderRepo

--- KoiOrderingSystem_Repository/Repo/OrderRepo.cs
+++ KoiOrderingSystem_Repository/Repo/OrderRepo.cs
@@ -7,10 +7,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOrderingSystem_DAO;
 
 namespace KoiOrderingSystem_Repository.Repo
 {
     public class OrderRepo : IOrderRepo
     {
+        public Task<bool> Add(Order model)
+        {
+            return OrderDAO.Instance.Add(model);
+        }
+
+        public Task<List<Order>> GetAll()
+        {
+            return OrderDAO.Instance.GetAll();
+        }
+
+        public Task<Order?> GetById(int id)
+        {
+            return OrderDAO.Instance.GetById(id);
+        }
+
+        public Task<bool> Remove(int id)
+        {
+            return OrderDAO.Instance.Remove(id);
+        }
+
+        public Task<bool> Update(Order model)
+        {
+            return OrderDAO.Instance.Update(model);
+        }
     }
 }
