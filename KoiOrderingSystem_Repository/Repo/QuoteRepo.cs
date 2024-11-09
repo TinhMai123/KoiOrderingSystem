56936f53e3bf252e793e0db3f89fc531fc74f44b
Shymastic <shymastic@gmail.com>
2024-11-09 18:50:58 +0700

    changing QuoteRepo

--- KoiOrderingSystem_Repository/Repo/QuoteRepo.cs
+++ KoiOrderingSystem_Repository/Repo/QuoteRepo.cs
@@ -7,10 +7,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOrderingSystem_DAO;
 
 namespace KoiOrderingSystem_Repository.Repo
 {
     public class QuoteRepo : IQuoteRepo
     {
+        public async Task<bool> Add(Quote model)
+        {
+            return await QuoteDAO.Instance.Add(model);
+        }
+
+        public async Task<List<Quote>> GetAll()
+        {
+            return await QuoteDAO.Instance.GetAll();
+        }
+
+        public async Task<Quote?> GetById(int id)
+        {
+            return await QuoteDAO.Instance.GetById(id);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await QuoteDAO.Instance.Remove(id);
+        }
+
+        public async Task<bool> Update(Quote model)
+        {
+            return await QuoteDAO.Instance.Update(model);
+        }
     }
 }
