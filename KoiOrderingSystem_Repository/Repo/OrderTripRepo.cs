7b517770888dda766d564bd977f70562ed651299
Shymastic <shymastic@gmail.com>
2024-11-09 18:48:34 +0700

    changing OrderTripRepo

--- KoiOrderingSystem_Repository/Repo/OrderTripRepo.cs
+++ KoiOrderingSystem_Repository/Repo/OrderTripRepo.cs
@@ -7,11 +7,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOrderingSystem_DAO;
 
 namespace KoiOrderingSystem_Repository.Repo
 {
     public class OrderTripRepo : IOrderTripRepo
     {
+        public async Task<bool> Add(OrderTrip model)
+        {
+            return await OrderTripDAO.Instance.Add(model);
+        }
 
+        public async Task<List<OrderTrip>> GetAll()
+        {
+            return await OrderTripDAO.Instance.GetAll();
+        }
+
+        public async Task<OrderTrip?> GetById(int id)
+        {
+            return await OrderTripDAO.Instance.GetById(id);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await OrderTripDAO.Instance.Remove(id);
+        }
+
+        public async Task<bool> Update(OrderTrip model)
+        {
+            return await OrderTripDAO.Instance.Update(model);
+        }
     }
 }
