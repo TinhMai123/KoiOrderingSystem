b84d618631bb64fe4e0b280b9d6a2ef58c0a7100
huyngse <ngogiahuy775309@gmail.com>
2024-11-12 13:18:20 +0700

    Layouts

---
+++ KoiOrderingSystem_Web/Pages/Customer/_layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiOrderingSystem_Web.Pages.Customer
+{
+    public class _layoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
