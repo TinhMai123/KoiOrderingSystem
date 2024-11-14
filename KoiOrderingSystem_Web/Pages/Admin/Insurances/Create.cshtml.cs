2ff5a790705c679fbe3077f3a9b06a3f91be998d
Shymastic <shymastic@gmail.com>
2024-11-14 21:24:07 +0700

    fixing Update for all DAO

---
+++ KoiOrderingSystem_Web/Pages/Admin/Insurances/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using KoiOrderingSystem_BusinessObject;
+using KoiOrderingSystem_BusinessObject.Data;
+
+namespace KoiOrderingSystem_Web.Pages.Admin.Insurances
+{
+    public class CreateModel : PageModel
+    {
+        private readonly KoiOrderingSystem_BusinessObject.Data.KoiOrderingSystemContext _context;
+
+        public CreateModel(KoiOrderingSystem_BusinessObject.Data.KoiOrderingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Insurance Insurance { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Insurances == null || Insurance == null)
+            {
+                return Page();
+            }
+
+            _context.Insurances.Add(Insurance);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ KoiOrderingSystem_Web/Pages/Staff/Consultant/OrderTrips/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KoiOrderingSystem_BusinessObject;
+using KoiOrderingSystem_BusinessObject.Data;
+
+namespace KoiOrderingSystem_Web.Pages.Staff.Consultant.OrderTrips
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KoiOrderingSystem_BusinessObject.Data.KoiOrderingSystemContext _context;
+
+        public IndexModel(KoiOrderingSystem_BusinessObject.Data.KoiOrderingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public IList<OrderTrip> OrderTrip { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.OrderTrips != null)
+            {
+                OrderTrip = await _context.OrderTrips
+                .Include(o => o.Consultant)
+                .Include(o => o.Farm)
+                .Include(o => o.Order).ToListAsync();
+            }
+        }
+    }
+}
